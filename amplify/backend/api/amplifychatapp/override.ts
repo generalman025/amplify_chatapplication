6aca66bf3011f8bc5a9fd0db038e91ac724a0b65
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-02 00:25:43 +0700

    fix: enable custom resource

--- amplify/backend/api/amplifychatapp/override.ts
+++ amplify/backend/api/amplifychatapp/override.ts
@@ -1,17 +1,5 @@
 import { AmplifyApiGraphQlResourceStackTemplate } from "@aws-amplify/cli-extensibility-helper";
-import { CfnWebACL } from "@aws-cdk/aws-wafv2";
 
 export function override(resources: AmplifyApiGraphQlResourceStackTemplate) {
   resources.api.GraphQLAPI.xrayEnabled = true;
-  const cfnWebACL = new CfnWebACL(this, 'WebACL', {
-      defaultAction: {
-          allow: { },
-      },
-      scope: resources.api.GraphQLAPI.attrArn,
-      visibilityConfig: {
-          cloudWatchMetricsEnabled: true,
-          metricName: 'waf',
-          sampledRequestsEnabled: false
-      }
-  });
 }
