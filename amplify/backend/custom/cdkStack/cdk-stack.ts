ba7fb1ef0ecf964839da2f2000dc45228d0b37d9
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-02 01:45:29 +0700

    fix: waf

--- amplify/backend/custom/cdkStack/cdk-stack.ts
+++ amplify/backend/custom/cdkStack/cdk-stack.ts
@@ -1,39 +1,52 @@
-import * as cdk from '@aws-cdk/core';
-import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
-import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
+import * as cdk from "@aws-cdk/core";
+import * as AmplifyHelpers from "@aws-amplify/cli-extensibility-helper";
+import { AmplifyDependentResourcesAttributes } from "../../types/amplify-dependent-resources-ref";
 import { CfnWebACL } from "@aws-cdk/aws-wafv2";
+import { CfnWebACLAssociation } from "@aws-cdk/aws-wafregional";
 
 export class cdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props?: cdk.StackProps,
+    amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps
+  ) {
     super(scope, id, props);
     /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
-    new cdk.CfnParameter(this, 'env', {
-      type: 'String',
-      description: 'Current Amplify CLI env name',
+    new cdk.CfnParameter(this, "env", {
+      type: "String",
+      description: "Current Amplify CLI env name",
     });
     /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
-    
-    const dependencies: AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(this,
-      amplifyResourceProps.category,
-      amplifyResourceProps.resourceName,
-      [{
-        category: "api",
-        resourceName: "amplifychatapp"
-      }] 
-    );
 
-    const apiArn = cdk.Fn.ref(dependencies.api.amplifychatapp.GraphQLAPIIdOutput)
+    const dependencies: AmplifyDependentResourcesAttributes =
+      AmplifyHelpers.addResourceDependency(
+        this,
+        amplifyResourceProps.category,
+        amplifyResourceProps.resourceName,
+        [
+          {
+            category: "api",
+            resourceName: "amplifychatapp",
+          },
+        ]
+      );
 
-    const cfnWebACL = new CfnWebACL(this, 'WebACL', {
+    const cfnWebACL = new CfnWebACL(this, "WebACL", {
       defaultAction: {
-          allow: { },
+        allow: {},
       },
       scope: "REGIONAL",
       visibilityConfig: {
-          cloudWatchMetricsEnabled: true,
-          metricName: 'waf',
-          sampledRequestsEnabled: false
-      }
+        cloudWatchMetricsEnabled: true,
+        metricName: "waf",
+        sampledRequestsEnabled: false,
+      },
+    });
+
+    const cfnWebACLAssociation = new CfnWebACLAssociation(this, 'WebACLAssociation', {
+      resourceArn: dependencies.api.amplifychatapp.GraphQLAPIIdOutput,
+      webAclId: cfnWebACL.ref
     });
   }
 }
