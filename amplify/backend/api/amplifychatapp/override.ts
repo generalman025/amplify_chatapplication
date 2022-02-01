b3fa13d0665bd09cc9e14e5693bf40afacdd9522
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-01 23:34:00 +0700

    fix: add waf

--- amplify/backend/api/amplifychatapp/override.ts
+++ amplify/backend/api/amplifychatapp/override.ts
@@ -1,7 +1,17 @@
-import { AmplifyApiGraphQlResourceStackTemplate } from '@aws-amplify/cli-extensibility-helper';
+import { AmplifyApiGraphQlResourceStackTemplate } from "@aws-amplify/cli-extensibility-helper";
+import { CfnWebACL } from "@aws-cdk/aws-wafv2";
 
 export function override(resources: AmplifyApiGraphQlResourceStackTemplate) {
-    resources.api.GraphQLAPI.xrayEnabled = true;
-    // resources.api.GraphQLAPI.attrArn
+  resources.api.GraphQLAPI.xrayEnabled = true;
+  const cfnWebACL = new CfnWebACL(this, 'WebACL', {
+      defaultAction: {
+          allow: { },
+      },
+      scope: resources.api.GraphQLAPI.attrArn,
+      visibilityConfig: {
+          cloudWatchMetricsEnabled: true,
+          metricName: 'waf',
+          sampledRequestsEnabled: false
+      }
+  });
 }
-
