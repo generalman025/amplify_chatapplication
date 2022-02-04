4d28d43bc172920f28bafdb37a0d264271fb7f69
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-05 02:22:31 +0700

    fix: csp

--- amplify/backend/custom/cdkStack/cdk-stack.ts
+++ amplify/backend/custom/cdkStack/cdk-stack.ts
@@ -43,6 +43,7 @@ export class cdkStack extends cdk.Stack {
         metricName: "waf",
         sampledRequestsEnabled: false,
       },
+      rules: awsManagedRules.map(wafRule => wafRule.rule)
     });
     
     const cfnWebACLAssociation = new CfnWebACLAssociation(this, 'WebACLAssociation', {
@@ -53,3 +54,131 @@ export class cdkStack extends cdk.Stack {
     cfnWebACLAssociation.node.addDependency(cfnWebACL);
   }
 }
+
+interface WafRule {
+  name: string;
+  rule: CfnWebACL.RuleProperty;
+}
+
+const awsManagedRules: WafRule[] = [
+  // AWS IP Reputation list includes known malicious actors/bots and is regularly updated
+  {
+      name: 'AWS-AWSManagedRulesAmazonIpReputationList',
+      rule: {
+      name: 'AWS-AWSManagedRulesAmazonIpReputationList',
+      priority: 10,
+      statement: {
+          managedRuleGroupStatement: {
+          vendorName: 'AWS',
+          name: 'AWSManagedRulesAmazonIpReputationList',
+          },
+      },
+      overrideAction: {
+          none: {},
+      },
+      visibilityConfig: {
+          sampledRequestsEnabled: true,
+          cloudWatchMetricsEnabled: true,
+          metricName: 'AWSManagedRulesAmazonIpReputationList',
+      },
+      },
+  },
+  // Common Rule Set aligns with major portions of OWASP Core Rule Set
+  {
+      name: 'AWS-AWSManagedRulesCommonRuleSet',
+      rule:
+      {
+      name: 'AWS-AWSManagedRulesCommonRuleSet',
+      priority: 20,
+      statement: {
+          managedRuleGroupStatement: {
+          vendorName: 'AWS',
+          name: 'AWSManagedRulesCommonRuleSet',
+          // Excluding generic RFI body rule for sns notifications
+          // https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-list.html
+            excludedRules: [
+             { name: 'GenericRFI_BODY' },
+             { name: 'SizeRestrictions_BODY' },
+          ],
+          },
+      },
+      overrideAction: {
+          none: {},
+      },
+      visibilityConfig: {
+          sampledRequestsEnabled: true,
+          cloudWatchMetricsEnabled: true,
+          metricName: 'AWS-AWSManagedRulesCommonRuleSet',
+      },
+      },
+  },
+  // Blocks common SQL Injection
+  {
+      name: 'AWSManagedRulesSQLiRuleSet',
+      rule: {
+      name: 'AWSManagedRulesSQLiRuleSet',
+      priority: 30,
+      visibilityConfig: {
+          sampledRequestsEnabled: true,
+          cloudWatchMetricsEnabled: true,
+          metricName: 'AWSManagedRulesSQLiRuleSet',
+      },
+      overrideAction: {
+          none: {},
+      },
+      statement: {
+          managedRuleGroupStatement: {
+          vendorName: 'AWS',
+          name: 'AWSManagedRulesSQLiRuleSet',
+          excludedRules: [],
+          },
+      },
+      },
+  },
+  // Blocks common PHP attacks such as using high risk variables and methods in the body or queries
+  {
+      name: 'AWSManagedRulePHP',
+      rule: {
+      name: 'AWSManagedRulePHP',
+      priority: 40,
+      visibilityConfig: {
+          sampledRequestsEnabled: true,
+          cloudWatchMetricsEnabled: true,
+          metricName: 'AWSManagedRulePHP',
+      },
+      overrideAction: {
+          none: {},
+      },
+      statement: {
+          managedRuleGroupStatement: {
+          vendorName: 'AWS',
+          name: 'AWSManagedRulesPHPRuleSet',
+          excludedRules: [],
+          },
+      },
+      },
+  },
+  // Blocks attacks targeting LFI(Local File Injection) for linux systems
+  {
+      name: 'AWSManagedRuleLinux',
+      rule: {
+      name: 'AWSManagedRuleLinux',
+      priority: 50,
+      visibilityConfig: {
+          sampledRequestsEnabled: true,
+          cloudWatchMetricsEnabled: true,
+          metricName: 'AWSManagedRuleLinux',
+      },
+      overrideAction: {
+          none: {},
+      },
+      statement: {
+          managedRuleGroupStatement: {
+          vendorName: 'AWS',
+          name: 'AWSManagedRulesLinuxRuleSet',
+          excludedRules: [],
+          },
+      },
+      },
+  },
+];
