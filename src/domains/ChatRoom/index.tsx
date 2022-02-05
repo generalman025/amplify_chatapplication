9b4563d94bfb686222df6689ba0fad8a2762670c
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-05 11:47:52 +0700

    fix: checking username

--- src/domains/ChatRoom/index.tsx
+++ src/domains/ChatRoom/index.tsx
@@ -1,14 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Grid, Paper } from '@mui/material';
 import AppBar from '../../components/AppBar';
 import ChatBox from '../../components/ChatBox';
 import UserListsBox from '../../components/UserListsBox';
 import Alert from '../../components/Alert';
 import { UtilContext } from '../../context/UtilContext';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function ChatRoom() {
+  const navigate = useNavigate();
+  const { username } = useContext(AuthContext);
   const { severity, alertMessage, showAlert, setShowAlert } =
     useContext(UtilContext);
+
+    useEffect(() => {
+      if(!username || username === ''){
+        navigate('/');
+      }
+    }, []);
 
   return (
     <Grid container justifyContent="center">
