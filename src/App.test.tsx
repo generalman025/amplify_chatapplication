1b76f3a24832459b603543dc820282c9bf32afb8
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-05 13:28:15 +0700

    feat: add cypress

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {MemoryRouter, BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 test('login page', () => {
   render(<Router>
