2610726ec97941cc2a093c579f1fa052651a99ef
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-05 12:47:28 +0700

    feat: add test phases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {MemoryRouter, BrowserRouter as Router} from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('login page', () => {
+  render(<Router>
+    <App/>
+    </Router>);
+  const linkElement = screen.getByText(/Proceed to Chat Room/i);
   expect(linkElement).toBeInTheDocument();
 });
