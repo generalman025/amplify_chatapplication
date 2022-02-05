b51f1e33fc967208e4c728528a75386b2a340f7f
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-05 15:37:15 +0700

    fix: ci

--- src/components/UsernameBox/index.test.tsx
+++ src/components/UsernameBox/index.test.tsx
@@ -1,22 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import { useContext } from 'react';
 import UsernameBox from '.';
 import { AuthContext } from '../../context/AuthContext';
-
-beforeAll(() => {
-    const {setUsername} = useContext(AuthContext);
-    setUsername('test');
-})
+import { AuthState } from '@aws-amplify/ui-components';
+import { createContext, ReactChild } from 'react';
 
 test('Should render a textbox', () => {
     render(<UsernameBox />);
     const linkElement = screen.getByLabelText(/Preferred Username/i);
     expect(linkElement).toBeInTheDocument();
-  });
-  
+});
+
 
 test('Should render a button', () => {
-  render(<UsernameBox />);
-  const linkElement = screen.getByText(/Proceed to Chat Room/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<UsernameBox />);
+    const linkElement = screen.getByText(/Proceed to Chat Room/i);
+    expect(linkElement).toBeInTheDocument();
 });
+
+const customRender = (ui: any, { providerProps, ...renderOptions }: any) => {
+    return render(
+        <AuthContext.Provider {...providerProps}>{ui}</AuthContext.Provider>,
+        renderOptions,
+    )
+}
+
+
+// test('Should render a default username', () => {
+//     jest.mock("../../context/AuthContext", () => ({
+//         __esModule: true,
+//         default: createContext({ username: 'test' })
+//     }));
+//     // render(<AuthContext.Provider value={{ username: 'test', user: null, authState: AuthState.SignedIn, setUsername: (() => true), setUser: (() => true), setAuthState: (() => true) }}><UsernameBox /></AuthContext.Provider>);
+//     customRender(<UsernameBox />, { username: 'test' });
+//     const linkElement = screen.getByLabelText(/test/i);
+//     expect(linkElement).toBeInTheDocument();
+// });
