51fcd2701879a4bd9f1971c179613b8eb603f6e0
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-05 15:01:32 +0700

    fix: ci

--- src/components/UsernameBox/index.test.tsx
+++ src/components/UsernameBox/index.test.tsx
@@ -1,18 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import { useContext } from 'react';
 import UsernameBox from '.';
+import { AuthContext } from '../../context/AuthContext';
 
+beforeAll(() => {
+    const {setUsername} = useContext(AuthContext);
+    setUsername('test');
+})
 
-test('Should render textbox', () => {
+test('Should render a textbox', () => {
     render(<UsernameBox />);
-    const linkElement = screen.getByText(/Preferred Username/i);
-    console.log(screen);
+    const linkElement = screen.getByLabelText(/Preferred Username/i);
     expect(linkElement).toBeInTheDocument();
   });
   
 
-test('Should render button', () => {
+test('Should render a button', () => {
   render(<UsernameBox />);
   const linkElement = screen.getByText(/Proceed to Chat Room/i);
-  console.log(screen);
   expect(linkElement).toBeInTheDocument();
 });
