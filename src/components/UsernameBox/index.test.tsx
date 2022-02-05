47bef9491ec00723b2650d1a0e3e5fd29992a181
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-05 14:42:57 +0700

    fix: ci

--- src/components/UsernameBox/index.test.tsx
+++ src/components/UsernameBox/index.test.tsx
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import UsernameBox from '.';
 
+
+test('Should render textbox', () => {
+    render(<UsernameBox />);
+    const linkElement = screen.getByText(/Preferred Username/i);
+    console.log(screen);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+
 test('Should render button', () => {
   render(<UsernameBox />);
   const linkElement = screen.getByText(/Proceed to Chat Room/i);
+  console.log(screen);
   expect(linkElement).toBeInTheDocument();
 });
