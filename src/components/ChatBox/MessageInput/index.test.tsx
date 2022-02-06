6a5fa4bb45b1f75a73aa6af4c78642f9a11ccb44
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-06 22:28:01 +0700

    fix: coverage

--- src/components/ChatBox/MessageInput/index.test.tsx
+++ src/components/ChatBox/MessageInput/index.test.tsx
@@ -4,13 +4,13 @@ import MessageInput from '.';
 test('Should display a placeholder', () => {
   const handleSubmit = jest.fn();
   render(
-    <MessageInput message="" setMessage={() => {}} handleSubmit={handleSubmit} />);
+    <MessageInput message="" setMessage={() => true} handleSubmit={handleSubmit} />);
   const input = screen.getByPlaceholderText('Type your message here...');
   expect(input).toBeInTheDocument();
 });
 
 test('test 2', () => {
-  const dom = render(<MessageInput message="" setMessage={() => {}} handleSubmit={() => {}} />);
+  const dom = render(<MessageInput message="" setMessage={() => true} handleSubmit={() => true} />);
   const input = dom.container.querySelector('#messageInput');
   fireEvent.change(input as Element, { target: { value: '5555555' } });
   // expect(screen.getByDisplayValue('Type your message here...')).toBe(true);
