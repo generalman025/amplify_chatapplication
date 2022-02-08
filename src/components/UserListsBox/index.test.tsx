81836cd3f14e09593b26d62581577abde1aeaf9a
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-08 17:23:34 +0700

    fix: unittest

--- src/components/UserListsBox/index.test.tsx
+++ src/components/UserListsBox/index.test.tsx
@@ -6,55 +6,55 @@ import UserListsBox from '.';
 describe('Unit Testing : UserListBox', () => {
   let realConsoleError: any;
   let realAPIGet: any;
-  beforeAll(() => {
-    realConsoleError = console.error;
-    console.error = jest.fn(); // suppress warnings
+//   beforeAll(() => {
+//     realConsoleError = console.error;
+//     console.error = jest.fn(); // suppress warnings
 
-    realAPIGet = API.get;
-  });
+//     realAPIGet = API.get;
+//   });
 
-  afterAll(() => {
-    console.error = realConsoleError;
+//   afterAll(() => {
+//     console.error = realConsoleError;
 
-    API.get = realAPIGet;
-  });
+//     API.get = realAPIGet;
+//   });
 
   test('Should display an alert', () => {
-    API.get = jest.fn().mockImplementation(() => {
-      return Promise.resolve({
-        users: [
-          {
-            Attributes: [
-              {
-                Name: 'preferred_username',
-                Value: 'test1'
-              }
-            ]
-          },
-          {
-            Attributes: [
-              {
-                Name: 'preferred_username',
-                Value: 'test2'
-              }
-            ]
-          },
-          {
-            Attributes: [
-              {
-                Name: 'preferred_username',
-                Value: 'test3'
-              }
-            ]
-          }
-        ]
-      });
-    });
+    // API.get = jest.fn().mockImplementation(() => {
+    //   return Promise.resolve({
+    //     users: [
+    //       {
+    //         Attributes: [
+    //           {
+    //             Name: 'preferred_username',
+    //             Value: 'test1'
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         Attributes: [
+    //           {
+    //             Name: 'preferred_username',
+    //             Value: 'test2'
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         Attributes: [
+    //           {
+    //             Name: 'preferred_username',
+    //             Value: 'test3'
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   });
+    // });
 
     let component: any;
+    component = mount(<UserListsBox />);
     waitFor(() => {
-      component = mount(<UserListsBox />);
-      component.setState({ allUsers: API.get });
+        component.setState({ allUsers: API.get });
     });
   });
 });
