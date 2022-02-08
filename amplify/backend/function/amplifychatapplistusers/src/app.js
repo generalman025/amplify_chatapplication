5c8fcf3542889073d952b94d280868f0be7f867d
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-08 16:46:49 +0700

    fix: cors

--- amplify/backend/function/amplifychatapplistusers/src/app.js
+++ amplify/backend/function/amplifychatapplistusers/src/app.js
@@ -28,7 +28,7 @@ app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  let allowedOrigins = ["https://chatapp.g025app.com, https://chatapp-dev.g025app.com"];
+  let allowedOrigins = ["https://chatapp.g025app.com", "https://chatapp-dev.g025app.com"];
   let origin = req.headers.origin;
   if(allowedOrigins.includes(origin)){
     res.header("Access-Control-Allow-Origin", origin)
