8a93f6e4357d6ca00e415985abcfd0da5150fed8
Kiattisak Chaisomboon <kiattisak_ch@tripetch-it.co.th>
2022-02-10 23:18:01 +0700

    fix: unit test

--- src/components/Alert/index.test.tsx
+++ src/components/Alert/index.test.tsx
@@ -1,40 +1,42 @@
+import { fireEvent } from '@testing-library/react';
+import { mount, shallow } from 'enzyme';
 import Alert, { SeverityType } from '.';
 
 describe('Unit Testing : Alert', () => {
   test('Should display an alert', () => {
-    const component = Alert({
-      showAlert: true,
-      alertMessage: '',
-      severity: SeverityType.success,
-      setShowAlert: jest.fn(),
-      onSuccess: jest.fn()
-    });
-    expect(component.props.open).toEqual(true);
+    const component = mount(<Alert
+      showAlert={true}
+      alertMessage=''
+      severity={SeverityType.success}
+      setShowAlert={jest.fn()}
+      onSuccess={jest.fn()}
+    />);
+    expect(component.debug()).toContain('SuccessOutlinedIcon');
   });
 
   test('Should call an onSuccess event', () => {
     const onSuccess = jest.fn();
-    const component = Alert({
-      showAlert: true,
-      alertMessage: '',
-      severity: SeverityType.success,
-      setShowAlert: jest.fn(),
-      onSuccess: onSuccess
-    });
-    component.props.onClose();
+    const component = shallow(<Alert
+      showAlert={true}
+      alertMessage=''
+      severity={SeverityType.success}
+      setShowAlert={jest.fn()}
+      onSuccess={onSuccess}
+    />);
+    component.simulate("close");
     expect(onSuccess).toBeCalled();
   });
 
   test('Should not call an onSuccess event on error', () => {
     const onSuccess = jest.fn();
-    const component = Alert({
-      showAlert: true,
-      alertMessage: '',
-      severity: SeverityType.error,
-      setShowAlert: jest.fn(),
-      onSuccess: onSuccess
-    });
-    component.props.onClose();
+    const component = shallow(<Alert
+      showAlert={true}
+      alertMessage=''
+      severity={SeverityType.error}
+      setShowAlert={jest.fn()}
+      onSuccess={onSuccess}
+    />);
+    component.simulate("close");
     expect(onSuccess).toBeCalledTimes(0);
   });
 });
